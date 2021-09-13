1ecdeb9bedd5b698e4cc30db8f3f65406b295cd1
krushik08 <krushik8980@email.com>
2021-09-13 18:00:25 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { Redirect, Route } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Blog from "./pages/Blog/Blog";
+import BlogDetails from "./pages/Blog/BlogDetails";
+import Cart from "./pages/Cart/Cart";
+import CheckOut from "./pages/CheckOut/CheckOut";
+import Error from "./pages/Error/Error";
+import HomePage from "./pages/HomePage/HomePage";
+import Product from "./pages/Product/Product";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+
+      <Route path="/" exact>
+        <Redirect to="/HomePage" />
+      </Route>
+      <Route path="/HomePage">
+        <HomePage />
+      </Route>
+
+      <Route path="/Product">
+        <Product />
+      </Route>
+      <Route path="/Blog">
+        <Blog />
+      </Route>
+      <Route path="/BlogDetails">
+        <BlogDetails />
+      </Route>
+      <Route path="/Cart">
+        <Cart />
+      </Route>
+      <Route path="/CheckOut">
+        <CheckOut />
+      </Route>
+      <Route path="/Error">
+        <Error />
+      </Route>
+
+      <Footer />
+    </>
   );
 }
 
